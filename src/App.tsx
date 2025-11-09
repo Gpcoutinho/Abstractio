a66262b8dabb03dcce8780cbdf213456fd2a2a11
Gpcoutinho <gpcoutinho17@gmail.com>
2025-11-09 18:46:47 -0300

    Muda nome da página Algorithms para Ponteiros

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Profile from './pages/Profile';
 import Tasks from './pages/Tasks';
-import Algorithms from './pages/Algorithms';
+import Ponteiros from './pages/Ponteiros';
 import { Routes, Route } from 'react-router-dom';
 
 
@@ -25,7 +25,7 @@ const App: React.FC = () => {
 
           <Route path="/tasks" element={<Tasks />} />
 
-          <Route path="/algorithms" element={<Algorithms />} />
+          <Route path="/ponteiros" element={<Ponteiros />} />
           
 
         </Routes>
